311f51b276abf9588ed08dcb16b211c5c953bd63
luisfaria <lfariaxxi@hotmail.com>
2020-06-04 09:26:34 +0100

    fix right side

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+import { Route, BrowserRouter, } from 'react-router-dom';
+
+import CountriesList from './components/CountriesList';
+import CountryDetail from './views/CountryDetail';
+
+import countries from './countries.json';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      
+        <CountriesList countries={countries}/>
+
+        <Route path="/country/:countryCode" component={CountryDetail} />
+      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
